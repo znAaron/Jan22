a78fdf5d610d28ffb205966a12dd44dabc05a941
Zinan Guo <guozinan2000@gmail.com>
2024-01-16 22:49:03 -0500

    Update loading screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
-import cake from './cake.png';
-import './App.css';
-import confetti from 'canvas-confetti';
+import "./App.css";
+import confetti from "canvas-confetti";
 
-const birthdate = '2001-01-22'
+const birthdate = "2001-01-22 00:00:00 EST";
 
 var count = 200;
 var defaults = {
-  origin: { y: 0.7 }
+  origin: { y: 0.7 },
 };
 
 function fire(particleRatio, opts) {
-  confetti(Object.assign({}, defaults, opts, {
-    particleCount: Math.floor(count * particleRatio)
-  }));
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
 }
 
 function setOffFirework() {
@@ -27,13 +28,13 @@ function setOffFirework() {
   fire(0.35, {
     spread: 100,
     decay: 0.91,
-    scalar: 0.8
+    scalar: 0.8,
   });
   fire(0.1, {
     spread: 120,
     startVelocity: 25,
     decay: 0.92,
-    scalar: 1.2
+    scalar: 1.2,
   });
   fire(0.1, {
     spread: 120,
@@ -41,15 +42,16 @@ function setOffFirework() {
   });
 }
 
-setOffFirework();
-
 function calculateAge(birthdate) {
   const today = new Date();
   const birthDate = new Date(birthdate);
   let age = today.getFullYear() - birthDate.getFullYear();
   const monthDiff = today.getMonth() - birthDate.getMonth();
 
-  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+  if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && today.getDate() < birthDate.getDate())
+  ) {
     age--;
   }
 
@@ -61,12 +63,9 @@ function calculateDaysUntilBirthday(birthdate) {
   const birthDate = new Date(birthdate);
   birthDate.setFullYear(today.getFullYear());
 
-  if (today > birthDate) {
-    birthDate.setFullYear(today.getFullYear() + 1);
-  }
-
+  
   const timeDiff = birthDate.getTime() - today.getTime();
-  const daysUntilBirthday = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  const daysUntilBirthday = Math.ceil(timeDiff / (1000 * 3600 *24));
 
   return daysUntilBirthday;
 }
@@ -75,7 +74,17 @@ function App() {
   const age = calculateAge(birthdate);
   const daysUntilBirthday = calculateDaysUntilBirthday(birthdate);
 
-  const [birthdayMessage, setBirthdayMessage] = useState(`Happy ${age}st Birthday!`);
+  if (daysUntilBirthday == 0) {
+    setOffFirework();
+  }
+
+  const [birthdayMessage, setBirthdayMessage] = useState(
+    `Happy ${age}st Birthday!`
+  );
+  const [graphic, setGraphic] = useState(`./cake.png`);
+  const [btnMessage, setBtnMessage] = useState(
+    `More Firework!`
+  );
 
   useEffect(() => {
     if (daysUntilBirthday !== 0) {
@@ -84,16 +93,24 @@ function App() {
     }
   }, [daysUntilBirthday]);
 
+  useEffect(() => {
+    if (daysUntilBirthday !== 0) {
+      setGraphic("./loading.gif");
+    }
+  }, [daysUntilBirthday]);
+
+  useEffect(() => {
+    if (daysUntilBirthday !== 0) {
+      setBtnMessage("Can't wait already!");
+    }
+  }, [daysUntilBirthday]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={cake} className="Cake" alt="Birthday Cake" />
-        <p>
-          {birthdayMessage}
-        </p>
-        <button onClick={setOffFirework}>
-          More Firework!
-        </button>
+        <img src={graphic} className="Cake" alt="Birthday Cake" />
+        <p>{birthdayMessage}</p>
+        <button onClick={setOffFirework}>{btnMessage}</button>
       </header>
     </div>
   );
